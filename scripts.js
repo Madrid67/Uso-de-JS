88595be2a8528365f1bf9e86cc2618ecdaacee79
Madrid67 <madrisito14@gmail.com>
2024-09-10 22:10:18 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+
+let edad = 25;
+let nombre = "Juan";
+let altura = 1.75;
+let peso = 70;
+let esEstudiante = true;
+let ciudad = "Madrid";
+let temperatura = 20;
+let numero1 = 10;
+let numero2 = 5;
+let promedio = (numero1 + numero2) / 2;
+
+
+const PI = 3.14159;
+const SALUDO = "Hola, ";
+const MAX_EDAD = 100;
+const TEMPERATURA_CONGELACION = 0;
+const NUMERO_MAGICO = 7;
+
+
+let resultadoSuma = numero1 + numero2; 
+let resultadoResta = numero1 - numero2; 
+let resultadoMultiplicacion = numero1 * numero2; 
+let resultadoDivision = numero1 / numero2; 
+let esMayorDeEdad = edad >= 18 ? "Es mayor de edad" : "Es menor de edad"; 
+
+console.log(SALUDO + nombre);
+console.log(`Edad: ${edad}`);
+console.log(`Altura: ${altura} m`);
+console.log(`Peso: ${peso} kg`);
+console.log(`Ciudad: ${ciudad}`);
+console.log(`Temperatura actual: ${temperatura}°C`);
+console.log(`Promedio: ${promedio}`);
+console.log(`Resultado de la suma: ${resultadoSuma}`);
+console.log(`Resultado de la resta: ${resultadoResta}`);
+console.log(`Resultado de la multiplicación: ${resultadoMultiplicacion}`);
+console.log(`Resultado de la división: ${resultadoDivision}`);
+console.log(esMayorDeEdad);
+console.log(`Número mágico: ${NUMERO_MAGICO}`);
+console.log(`Valor de PI: ${PI}`);
